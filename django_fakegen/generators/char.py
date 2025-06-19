013964a184a6927c20a9ebed57c96c956582bea0
Moataz0000 <moatazfawzy150@gmail.com>
2025-06-19 20:25:41 +0300

    fix: fix bug

--- django_fakegen/generators/char.py
+++ django_fakegen/generators/char.py
@@ -5,10 +5,10 @@ from django.db import models
 
 class CharFieldGenerator(BaseFieldGenerator):
 
-    def can_handle(self, field_type: str) -> bool:
-        return field_type.get_internal_type in ('CharField', 'TextField')
+    def can_handle(self, field) -> bool:
+        return field.get_internal_type() in ("CharField", "TextField")
     
 
     def generate(self, field, faker, registry) -> None:
-        max_length = getattr(field, 'max_length', 50)
+        max_length = getattr(field, "max_length", 50)
         return faker.text(max_nb_chars=min(max_length, 200))
